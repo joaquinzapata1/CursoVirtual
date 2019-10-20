c5bc3cca045b2bbebd0c457e26e6fad9894e6ea8
Joaquin Zapata <joaquin@sinequix.com>
2019-10-20 15:38:06 -0300

    Updates

---
+++ precache-manifest.9f01a9bf5700aeb775d1a063b9ccd00c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36628d37a22b606d19cf7aea1833a7c3",
+    "url": "/CursoVirtual/index.html"
+  },
+  {
+    "revision": "08e283ecfd3a2c8aedeb",
+    "url": "/CursoVirtual/static/css/main.b0594376.chunk.css"
+  },
+  {
+    "revision": "dc41a40334a26b25ea38",
+    "url": "/CursoVirtual/static/js/2.3e714236.chunk.js"
+  },
+  {
+    "revision": "08e283ecfd3a2c8aedeb",
+    "url": "/CursoVirtual/static/js/main.cec7cbfc.chunk.js"
+  },
+  {
+    "revision": "560f488ec93e1d0170a6",
+    "url": "/CursoVirtual/static/js/runtime-main.9990a04d.js"
+  },
+  {
+    "revision": "15c73074346b77037d01ef659571955f",
+    "url": "/CursoVirtual/static/media/tutor-circ.15c73074.png"
+  },
+  {
+    "revision": "57c5ea2774bb5aa5771e891769e459f5",
+    "url": "/CursoVirtual/static/media/tutor.57c5ea27.png"
+  }
+]);
